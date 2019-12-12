f26ea96c2847e19316bddbe15e3e0d4745688b64
rajaansuggs <42246673+rajaansuggs@users.noreply.github.com>
2019-12-12 12:17:49 -0500

    Update odd_parity.v

--- odd_parity.v
+++ odd_parity.v
@@ -1,2 +1,16 @@
 module odd_parity(clk, rst, a);
 wire a;
+wire parity;
+  parameter
+  PARITY
+  NOTPARITY
+always(Posedge clk or negedge rst)
+  if(rst==1'b0)
+    parity=1'b0
+    else if(rst==1'b1 and enParity==1'b0)
+    parity=1'b0
+      else 
+        
+      
+      
+    
