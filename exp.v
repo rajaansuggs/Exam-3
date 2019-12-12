b55ac9edefeedc8cdf79239483089e2cc1d90ddc
rajaansuggs <42246673+rajaansuggs@users.noreply.github.com>
2019-12-12 18:07:57 -0500

    Update exp.v

--- exp.v
+++ exp.v
@@ -1,15 +1,33 @@
 module exp(clk, rst, x, n)
+wire S, NS
+reg S
+reg NS
 parameter
-INIT
-COND
-ITER
-always(posedge clk or negedge rst)
+  INIT=2'd0
+  COND=2'd1
+  ITER=2'd2
+always @ (posedge clk or negedge rst)
 if(rst=2'b0)
-NS=INIT
+  NS=INIT
 else
-NS=S
+  NS=S
 always(*)
-case(*)
+  case(S)
   INIT:
   COND:
-  ITER:
+  ITER: S=
+    always @ (posedge rst or negedge rst)
+begin
+	if (rst == 1'd0)
+	begin
+		INIT <= 11'd0;
+		COND<= 11'd0;
+		 <= 11'd0;
+		S3 <= 11'd0;
+		S4 <= 11'd0;
+		S5 <= 11'd0;
+		S6 <= 11'd0;
+		S7 <= 11'd0;
+		S8 <= 11'd0;
+		S9 <= 11'd0;
+	end
