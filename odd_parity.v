4e43727a2f61283069c0d43f7966e8e2867a2ffa
rajaansuggs <42246673+rajaansuggs@users.noreply.github.com>
2019-12-12 21:54:36 -0500

    Update odd_parity.v

--- odd_parity.v
+++ odd_parity.v
@@ -5,9 +5,10 @@ assign apple = a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9]+a[10]+a[11]+a[1
   always@(*)begin
     if(start == 1'b1)
       begin
-        if(apple%2!=0)
+        if(apple%2!=0)begin
           done=1'b1;
           enable=1'b1;
+        end
       end
   end
 endmodule
