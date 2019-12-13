15b671e702b6c3959b91d7b877d760a4ec90d275
rajaansuggs <42246673+rajaansuggs@users.noreply.github.com>
2019-12-12 19:13:38 -0500

    Update exp.v

--- exp.v
+++ exp.v
@@ -1,33 +1,43 @@
-module exp(clk, rst, x, n)
-wire S, NS
-reg S
-reg NS
+module exp(clk, rst, c, j)
+wire S, NS;
+reg [15:0]exp=16'b0;
+reg [15:0]temp;
+reg [1:0]S=2'b0;
+reg [1:0]NS;	
+assign p=0;
 parameter
   INIT=2'd0
   COND=2'd1
   ITER=2'd2
-always @ (posedge clk or negedge rst)
-if(rst=2'b0)
-  NS=INIT
-else
-  NS=S
-always(*)
-  case(S)
-  INIT:
-  COND:
-  ITER: S=
-    always @ (posedge rst or negedge rst)
+	
+always @ (posedge clk)
 begin
-	if (rst == 1'd0)
-	begin
-		INIT <= 11'd0;
-		COND<= 11'd0;
-		 <= 11'd0;
-		S3 <= 11'd0;
-		S4 <= 11'd0;
-		S5 <= 11'd0;
-		S6 <= 11'd0;
-		S7 <= 11'd0;
-		S8 <= 11'd0;
-		S9 <= 11'd0;
+  S<=NS;
+end
+always @(posedge clk)
+begin
+  case(S)
+	INIT:begin 
+		  if(start == 1'b1)
+		  begin
+			  temp<= c; //C
+			  exp<= 16'd0;
+			  NS<= COND; 
+		  end
+		  else NS<=2'd0;
 	end
+  	COND:begin
+		if(temp<j)begin
+			exp<=(exp+1'b1);
+			temp<= (temp*c);
+			NS<=COND;
+		end
+		  else NS<=ITER;
+	end
+  	ITER: begin
+		g<=exp;
+		done<=1'b1;
+	end
+	endcase
+end
+endmodule
