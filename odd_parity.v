77b1f645fe38aed6b8ea0543d524d713559dde85
rajaansuggs <42246673+rajaansuggs@users.noreply.github.com>
2019-12-12 23:07:25 -0500

    Update odd_parity.v

--- odd_parity.v
+++ odd_parity.v
@@ -1,6 +1,7 @@
 module odd_parity(start, done, a, enable);
 wire a;
-wire op;  
+wire op;
+output enable;
 assign op = a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9]+a[10]+a[11]+a[12]+a[13]+a[14]+a[15];
   always@(*)begin
     if(start == 1'b1)
