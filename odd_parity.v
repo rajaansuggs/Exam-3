a4b28ce13aa9e5faa5b7c965279844f66df066ef
rajaansuggs <42246673+rajaansuggs@users.noreply.github.com>
2019-12-12 21:35:42 -0500

    Update odd_parity.v

--- odd_parity.v
+++ odd_parity.v
@@ -1,16 +1,12 @@
-module odd_parity(clk, rst, a);
+module odd_parity(start, done, a);
 wire a;
-wire parity;
-  parameter
-  PARITY
-  NOTPARITY
-always(Posedge clk or negedge rst)
-  if(rst==1'b0)
-    parity=1'b0
-    else if(rst==1'b1 and enParity==1'b0)
-    parity=1'b0
-      else 
+wire apple;  
+assign apple = a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9]+a[10]+a[11]+a[12]+a[13]+a[14]+a[15];
+  always@(*)
+    if(start = 1'b1)
+      begin
         
+    
       
       
     
