d7c2aa244f0ba12393618e1e3ade3f2fa2d35836
rajaansuggs <42246673+rajaansuggs@users.noreply.github.com>
2019-12-12 21:41:16 -0500

    Update odd_parity.v

--- odd_parity.v
+++ odd_parity.v
@@ -3,9 +3,10 @@ wire a;
 wire apple;  
 assign apple = a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9]+a[10]+a[11]+a[12]+a[13]+a[14]+a[15];
   always@(*)
-    if(start = 1'b1)
+    if(start == 1'b1)
       begin
-        
+        if(apple/2==0)
+          
     
       
       
