474816494f2e39329dc5b5014d27ca78992a18da
rajaansuggs <42246673+rajaansuggs@users.noreply.github.com>
2019-12-12 21:52:03 -0500

    Update odd_parity.v

--- odd_parity.v
+++ odd_parity.v
@@ -1,4 +1,4 @@
-module odd_parity(start, done, a);
+module odd_parity(start, done, a, enable);
 wire a;
 wire apple;  
 assign apple = a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9]+a[10]+a[11]+a[12]+a[13]+a[14]+a[15];
@@ -7,6 +7,7 @@ assign apple = a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9]+a[10]+a[11]+a[1
       begin
         if(apple%2!=0)
           done=1'b1;
+          enable=1'b1;
       end
   end
 endmodule
